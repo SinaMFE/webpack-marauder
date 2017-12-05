80cb2db563926d3f9c5e06b09f3361a5b33423e4
Vincent Lee <vv314@foxmail.com>
2017-12-05 18:20:47 +0800

    update

--- build/utils/utils.js
+++ build/utils/utils.js
@@ -32,7 +32,7 @@ function localIp() {
   const ip = devIp()
   // vpn 下 ip 为数组，第一个元素为本机局域网 ip
   // 第二个元素为 vpn 远程局域网 ip
-  return Array.isArray(ip) ? ip[0] : ip
+  return ip ? (Array.isArray(ip) ? ip[0] : ip) : '0.0.0.0'
 }
 
 /**
